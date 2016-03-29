1b2db51fe1d9a364e23d8551557e9ca924e55e6b
James Kolb (Ardagnir) <jck1089@gmail.com>
2016-03-28 23:40:31 -0400

    Added tests

--- test/charread.sh
+++ test/charread.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 text=$(cat $2)
 for (( i=0; i<${#text}; i++ )); do
-  echo -n "${text:$i:1}"
+  if [ "${text:$i:1}" == "" ]; then
+    # Type arrows without pauses
+    echo -n "${text:$i:3}"
+    i=$((i+2))
+  else
+    echo -n "${text:$i:1}"
+  fi
   sleep $1
 done
 echo ""
--- test/shell/inst1.sh
+++ test/shell/inst1.sh
@@ -1,2 +1,1 @@
 echo "basic test" > out1
-
