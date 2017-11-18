6a813c10e9730f923a9a5a0814f4925ae50622da
James Kolb (Ardagnir) <jck1089@gmail.com>
2017-11-18 11:02:04 -0500

    Fix tests for shells with old-style vi-mode

--- test/vim/inst6.sh
+++ test/vim/inst6.sh
@@ -1,4 +1,4 @@
 # Enter ex mode and make sure Athame pulls us out after enough keys.
 rm out6
-echo  Qohno
-ex  mode                 we made it > out6
+echo Qohno
+ex  mode             [C[C[C[C[C[C[Cwe made it > out6
